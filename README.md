2454a99986722222db7904a4d827be6b8788cb9b
NightRainMilkyWay <nightrainmilkyway@gmail.com>
2024-02-24 19:46:00 +0800

    更新内容

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 个人博客
 
-https://blog.mo7.cc
+https://nightrainmilkyway.cn/
 
 博客源码，基于 `VuePress Theme Hope` 搭建
 
@@ -24,49 +24,5 @@ https://bailemi.com/
 
 https://zhuanlan.zhihu.com/p/577560145
 
-## 评论服务
 
-文档\
-https://waline.js.org
-
-服务地址\
-https://talk.mo7.cc
-
-管理后台(Github 登录)
-https://talk.mo7.cc/ui
-
-## 图标
-
-https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.22&manage_type=myprojects&projectId=3855310
-
-## 运行
-
-编译环境: Debian 11 bullseye
-
-node 版本: v18 LTS
-
-```bash
-
-# 启用 pnpm
-npm install -g pnpm
-
-# 安装依赖
-pnpm install
-
-# 更新依赖
-pnpm run update
-
-# 运行
-pnpm run dev
-
-# 编译打包
-pnpm run build
-
-# 更新同步
-pnpm run sync
-
-# 发布到 https://blog.mo7.cc
-pnpm run deploy
-
-```
 # Blog
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,60 +1,21 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  // vue
-  "/zh/Vue/Vue/": [{ text: "Vue基础教程", icon: 'vue', link: "", children: "structure" }],
-  "/zh/Vue/VueCompositonApi/": [{ text: "CompositionApi", icon: 'API', prefix: "", children: "structure" }],
-  "/zh/Vue/VueRouter/": [{ text: "Vue路由管理", icon: "daohang", prefix: "", children: "structure" }],
-  "/zh/Vue/VueTransition/": [{ text: "Vue动画", icon: "donghua", prefix: "", children: "structure" }],
-  "/zh/Vue/VueX/": [{ text: "VueX状态管理", icon: "vuex-store", prefix: "", children: "structure" }],
-  "/zh/Vue/Pinia/": [{ text: "Pinia状态管理", icon: "boluo", prefix: "", children: "structure" }],
-  "/zh/Vue/VueProjectdeployment/": [{ text: "Vue项目开发与部署", icon: "bushu", prefix: "", children: "structure" }],
-  "/zh/Vue/VueBottom/": [{ text: "Vue底层机制", icon: "gongzuoliugongzuoliuguanli", prefix: "", children: "structure" }],
-
-
-  // react-gg
-  "/zh/React-gg/React-Basics/": [{ text: 'React基础', icon: 'jichushuju', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-state/": [{ text: '三大核心:state', icon: 'zhuangtai', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-props/": [{ text: '三大核心:props', icon: 'jisuchuandi', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-refs/": [{ text: '三大核心:refs', icon: 'icon', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-event/": [{ text: '受控组件和非受控组件', icon: 'kongzhizhongxin', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-senior/": [{ text: '高阶函数与柯里化', icon: 'hanshu', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-life/": [{ text: '组件的生命周期', icon: 'shengmingzhouqi', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-cli/": [{ text: 'react应用(基于react脚手架)', icon: 'jiaoshoujia-xian', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-ajax/": [{ text: 'react发送网络请求', icon: 'wangluo', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-router/": [{ text: 'react路由管理', icon: 'luyouqi', prefix: "", children: "structure" }],
-  "/zh/React-gg/React-project/": [{ text: 'React完整创建项目', icon: 'shizhanpeixun', prefix: "", children: "structure" }],
-
-
-  // react-wy
-  "/zh/React-wy/React/": [{ text: 'React 基础语法', icon: 'mofayufake', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-com-foundation/": [{ text: '组件化开发基础', icon: 'wangluo', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-state-primary/": [{ text: 'React状态入门', icon: 'daohang01', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-from/": [{ text: 'Reac处理表单事件', icon: '17', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-by-effect/": [{ text: 'React组件的副作用(生命周期)', icon: 'shengming', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-jsx/": [{ text: 'JSX 进阶', icon: 'jsx', prefix: "", children: "structure" }],
-  "/zh/React-wy/React-props/": [{ text: 'Props 进阶', icon: 'props', prefix: "", children: "structure" }],
-
-
-
-  // js css vue
-  "/zh/blend/ios/": [{ text: "ios杂项", icon: 'ios', prefix: "", children: "structure" }],
-  "/zh/blend/js/": [{ text: "js杂项", icon: 'js', prefix: "", children: "structure" }],
-  "/zh/blend/css/": [{ text: "css杂项", icon: 'CSS', prefix: "", children: "structure" }],
-  "/zh/blend/vue/": [{ text: "vue杂项", icon: 'vue', prefix: "", children: "structure" }],
-  "/zh/blend/npm/": [{ text: "npm杂项", icon: 'npm', prefix: "", children: "structure" }],
-  "/zh/blend/server/": [{ text: "服务器杂项", icon: 'fuwuqi', prefix: "", children: "structure" }],
-
-
-  // ts
-  "/zh/TypeScript/Ts_Basics/": [{ text: "Ts基础入门", icon: 'typescript', prefix: "", children: "structure" }],
-  "/zh/TypeScript/Vue_and_Ts/": [{ text: "Vue-and-Ts", icon: 'AwesomeVue', prefix: "", children: "structure" }],
-
-
-  // 面试题
-  "/zh/interview/JavaScript/": [{ text: "javaScript面试题", icon: 'mianshianpai', prefix: "", children: "structure" }],
-
-
-  // 小王同学的笔记
-  "/zh/wangjing/ps/": [{ text: "ps笔记", icon: 'ps', prefix: "", children: "structure" }],
+  "/": [
+    "",
+    {
+      text: "博客相关",
+      icon: "blog",
+      prefix: "blog/",
+      link: "blog/",
+    },
+    {
+      text: "关于",
+      icon: "info",
+      prefix: "about/",
+      link: "about",
+    },
+  ],
+  "/Arch Linux/": "structure",
+  "/Cu/": "structure",
 });
